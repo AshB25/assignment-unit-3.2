c2a83a09c817506d645eb1627bdd3d3adb950f10
Ashleigh Carter <ashbcarter@gmail.com>
2023-12-08 23:47:21 -0500

    countdown stretch goal

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -31,6 +31,9 @@ for(let i = 2; i <= 10; i+=2) {
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+for(let i = 5; i >= 0; i--) {
+  console.log('Countdown', i);
+}
 
 
 // 2. For of loops  
